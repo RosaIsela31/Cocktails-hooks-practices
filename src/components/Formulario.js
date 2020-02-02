5746916ad1ba67a7a602524e54d8b5fa3da27de3
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-02-01 21:52:43 -0600

    adding two context

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,76 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { CategoriasContext } from '../context/CategoriasContext';
+import { RecetasContext } from '../context/RecetasContext';
 
 const Formulario = () => {
+
+  const [ busqueda, guardarBusqueda ] = useState({
+    nombre: '',
+    categoria: '',
+  });
+
+  const { categorias } = useContext(CategoriasContext);  
+  const { buscarRecetas } = useContext(RecetasContext);
+
+  // Función para leer los contenidos 
+
+  const obtenerDatosReceta = e => {
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name] : [e.target.value]
+    })
+
+  };
+
   return ( 
-    <h1>Formulario</h1>
+    <form
+      className='col-12'
+      onSubmit={e => {
+          e.preventDefault();
+          buscarRecetas(busqueda)
+      }}
+    >
+      <fieldset className='text-center'>
+        <legend>Busca bebidas por Categoría o Ingrediente</legend>
+      </fieldset>
+
+      <div className='row mt-4'>
+        <div className='col-md-4'>
+          <input 
+            name='nombre'
+            className='form-control'
+            type='text'
+            placeholder='Buscar por Ingrediente'
+            onChange={obtenerDatosReceta}
+          />
+        </div>
+        <div className='col-md-4'>
+          <select 
+            className='form-control'
+            name='categoria'
+          >
+            <option value=''>-- Selecciona Categoría --</option>
+            {categorias.map(categoria => (
+               <option 
+                  key={categoria.strCategory} 
+                  value={categoria.strCategory} 
+               >
+               {categoria.strCategory}
+               </option>
+            ))}
+          </select>
+        </div>
+        <div className='col-md-4'>
+          <input
+            type='submit'
+            className='btn btn-block btn-primary' 
+            value='Buscar Bebidas'
+            onChange={obtenerDatosReceta}
+          />
+
+        </div>
+      </div>
+    </form>
    );
 }
  
